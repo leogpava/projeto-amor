63d124c50ef0a99960187f03ea3d52cc7c98ae4b
Leonardo Pavanelli <leogpava@gmail.com>
2025-03-25 13:58:35 -0300

    V.3.6
    Fixing

--- assets/javascript/pagescript.js
+++ assets/javascript/pagescript.js
@@ -9,6 +9,7 @@ let video = document.getElementById("video")
 
 function clicar() {
 if (data.value == "2023-04-28") {
+    data.value = ""
     sucesso.innerHTML = `<p>Date encontrado com sucesso! 👇</p>` 
     article.style.display = "block";
     secdate.style.display = "block";
@@ -19,11 +20,14 @@ if (data.value == "2023-04-28") {
     texto.innerHTML = `<p>Esse foi o dia em que nos vimos pessoalmente pela primeira vez.</p> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum repudiandae repellendus in optio eaque, deleniti rem deserunt voluptates illum accusamus veritatis enim iusto nesciunt quaerat perferendis? Eos iste ab placeat.</p>  <p>Aqui temos o único registro decente desse dia:</p>` 
     imagem.src = ""
 } else if (data.value == "2025-03-21") {
+    data.value = ""
     sucesso.innerHTML = `<p>Date encontrado com sucesso! 👇</p>` 
 
 } else if (data.value == "") {
+    data.value = ""
     window.alert("[ERRO] Inserir Data")
 } else {
+    data.value = ""
     secdate.style.display = "none";
     sucesso.innerHTML = `<p>Date não encontrado.</p>` 
 }
